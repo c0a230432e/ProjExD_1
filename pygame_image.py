bcf9a5889bdee7175d150c7e5963ce5604a10ff8
kanomata <c0a230432e@edu.teu.ac.jp>
2024-09-24 16:04:29 +0900

    練習７：背景画像ループ

--- pygame_image.py
+++ pygame_image.py
@@ -12,16 +12,20 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False) #練習2
+    bg_fl_img = pg.image.load("fig/pg_bg.jpg")
+    bg_fl_img = pg.transform.flip(bg_fl_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x = -(tmr%800)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
+        screen.blit(bg_fl_img, [x+1600, 0])
+        screen.blit(bg_img, [x+3200, 0])
         screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
-        clock.tick(200) #練習5
+        clock.tick(400) #練習5
 
 
 if __name__ == "__main__":
